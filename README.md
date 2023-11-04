5fd567ac3cbc094e0d38d0b93ee3963863515c46
Naveen RS <rnaveen.tkt@gmail.com>
2023-11-04 13:38:58 +0530

    updated app with html templates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Conversation_with_PDF_Summary
 Worked with both both Open AI API and HuggingFace API for PDF Text Extraction and summarization in chat format and ask queries related to the PDF
+
+Used Local Cache files for text embedding model from Hugging Face
---
+++ htmlTemplates.py
@@ -0,0 +1,45 @@
+import os
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = r'''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="{{ os.path.join('images', 'macaw.jpg') }}" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = r'''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src={{ os.path.join('images', 'user.jpg') }}>
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
